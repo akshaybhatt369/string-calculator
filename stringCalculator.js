7e7e2d44b3c7685dce0b51e60dac09956f0d577b
Akshay <akshaybhatt8108@gmail.com>
2025-06-11 16:53:58 +0530

    Return same number as input

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,4 +1,8 @@
 function add(numbers){
 if(numbers.length <=0) return 0;
+
+let delimiter = /,|\n/;
+const values = numbers.split(delimiter)
+if (values.length === 1) return parseInt(values.join(""))
 }
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,4 +4,8 @@ describe('String Calculator', () => {
   test('returns 0 for an empty string', () => {
     expect(add('')).toBe(0);
   });
+
+    test('returns same number', () => {
+    expect(add('5000')).toBe(5000);
+  });
 })
