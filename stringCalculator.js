048cc966225d39d53f3fa6c48629edb50483b1c3
Akshay <akshaybhatt8108@gmail.com>
2025-06-11 17:18:55 +0530

    Negative number test case

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,9 +1,15 @@
-function add(numbers){
-if(numbers.length <=0) return 0;
+function add(numbers) {
+  if (numbers.length <= 0) return 0;
 
-let delimiter = /,|\n/;
-const values = numbers.split(delimiter)
-if (values.length === 1) return parseInt(values.join(""))
-else return  values.map(Number).reduce((acc,num) => acc + num)
+  let delimiter = /,|\n/;
+  const values = numbers.split(delimiter);
+  const listOfNumbers = values.map(Number);
+  const negativeNumberList = listOfNumbers.filter((num) => num < 0);
+  if (negativeNumberList.length)
+    throw new Error(
+      `negative numbers not allowed <${negativeNumberList.join(",")}>`
+    );
+  if (values.length === 1) return parseInt(values.join(""));
+  else return values.map(Number).reduce((acc, num) => acc + num);
 }
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,16 +1,27 @@
-const add = require('./stringCalculator')
+const add = require("./stringCalculator");
 
-describe('String Calculator', () => {
-  test('returns 0 for an empty string', () => {
-    expect(add('')).toBe(0);
+describe("String Calculator", () => {
+  test("returns 0 for an empty string", () => {
+    expect(add("")).toBe(0);
   });
 
-    test('returns same number', () => {
-    expect(add('5000')).toBe(5000);
+  test("returns same number", () => {
+    expect(add("5000")).toBe(5000);
   });
 
-   test('returns addition of 2 number', () => {
-    expect(add('5000,1000')).toBe(6000);
+  test("returns addition of 2 number", () => {
+    expect(add("5000,1000")).toBe(6000);
   });
 
-})
+  test("returns error when negative number present", () => {
+    expect(() => add("5000,1000, -4000")).toThrow(
+      "negative numbers not allowed <-4000>"
+    );
+  });
+
+  test("returns error when more than one negative number present", () => {
+    expect(() => add("-28, 5000,1000, -4000")).toThrow(
+      "negative numbers not allowed <-28,-4000>"
+    );
+  });
+});
